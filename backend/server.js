19d31a45617eeaaeaff816427f21fbc8934d6f65
Ryan Lazzareschi <57603530+ryanlazz16@users.noreply.github.com>
2021-10-18 12:33:41 -0700

    Prepare for deployment to heroku

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import info from './data/info.js';
 
 dotenv.config();
 
 const app = express();
-
-app.get('/', (req, res) => {
-	res.send('API is running...');
-});
 
 app.get('/api/info', (req, res) => {
 	res.json(info);
@@ -21,6 +18,22 @@ app.get('/api/charities', (req, res) => {
 app.get('/api/nfts', (req, res) => {
 	res.json(info.nfts);
 });
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/nft_website/build')));
+
+	app.get('*', (req, res) => {
+		res.sendFile(
+			path.resolve(__dirname, 'nft_website', 'build', 'index.html')
+		);
+	});
+} else {
+	app.get('/', (req, res) => {
+		res.send('API is running...');
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
